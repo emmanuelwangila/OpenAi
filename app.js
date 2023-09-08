11e6b50cef87d1fb70753009af473b24e9b3124c
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-09-08 14:03:17 +0300

    connectiong Open Ai to root project api

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello manuel')
+
+const readline = require('readline');
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdou
+})
+
+
+rl.question('Youtube video title\n ', (title) => console.log(title));
