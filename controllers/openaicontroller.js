33abedb8b77764b73c4d17b1031f7b370589d3ab
emmanuelwangila <emmanuelwangila1@gmail.com>
2023-09-13 11:49:00 +0300

    async func openAi

---
+++ controllers/openaicontroller.js
@@ -0,0 +1,22 @@
+const openai = require('.../config/OpenaiConfig')
+
+
+const generateMeta = async (title) => {
+
+    const  description = await openai.createChatcompletion({
+        model:'gpt-3.5-turbo',
+        message: [
+            {
+                role: 'user',
+                content: `Come up with a description for a Youtube video called ${title}`
+            }
+        ],
+        max_tokens: 100
+
+    })
+
+    console.log(description.data.choices[0].message)
+
+}
+
+module.exports = {generateMeta}
